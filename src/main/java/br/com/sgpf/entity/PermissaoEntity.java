d875581795002f26ae27025e29bcd40e890a5d6f
Aluno <Aluno@ETEJK-LAB03-M11>
2024-05-08 21:20:08 -0300

    Implementado segurança

---
+++ src/main/java/br/com/sgpf/entity/PermissaoEntity.java
@@ -0,0 +1,67 @@
+package br.com.sgpf.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "permissao" , schema = "sgpf")
+public class PermissaoEntity implements Serializable,GrantedAuthority {
+	
+	private static final long serialVersionUID = 1L;	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_permissao")
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	
+	
+	@ManyToMany(mappedBy = "permissoes")
+    private List<UsuarioEntity> usuarios;	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public List<UsuarioEntity> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<UsuarioEntity> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+	@Override
+	public String getAuthority() {
+		
+		return this.nome;
+	}
+	
+}
