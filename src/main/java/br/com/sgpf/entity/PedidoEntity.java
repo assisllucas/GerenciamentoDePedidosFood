acbb0941932e42ad7d8772365f63fe82690a4adc
Aluno <Aluno@ETEJK-LAB03-M07>
2024-05-15 21:31:24 -0300

    UHUUUUL

--- src/main/java/br/com/sgpf/entity/PedidoEntity.java
+++ src/main/java/br/com/sgpf/entity/PedidoEntity.java
@@ -1,5 +1,73 @@
 package br.com.sgpf.entity;
 
-public class PedidoEntity {
+import java.sql.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+public class PedidoEntity {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idpedido")
+	private long idPedido;
+
+	@Column(name = "totalpedido")
+	private String totalPedido;
+	
+	@Column(name = "statuspedido")
+	private String statusPedido;
+	
+	@Column(name = "datapedido")
+	private Date dataPedido;
+
+	@OneToOne()
+	@JoinColumn(name = "cliente_idCliente", referencedColumnName = "idcliente")
+	private ClienteEntity cliente;
+
+	public long getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public String getTotalPedido() {
+		return totalPedido;
+	}
+
+	public void setTotalPedido(String totalPedido) {
+		this.totalPedido = totalPedido;
+	}
+
+	public String getStatusPedido() {
+		return statusPedido;
+	}
+
+	public void setStatusPedido(String statusPedido) {
+		this.statusPedido = statusPedido;
+	}
+
+	public Date getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(Date dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public ClienteEntity getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(ClienteEntity cliente) {
+		this.cliente = cliente;
+	}
+	
 }
